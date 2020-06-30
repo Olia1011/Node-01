72757e4c63dfb9d1091b7ec9168ae7f1e90334de
Olia1011 <olgamelnyk10111989@gmail.com>
2020-06-30 13:45:09 +0300

    commit3

--- index.js
+++ index.js
@@ -5,41 +5,7 @@ const uuid = require("uuid");
 const morgan = require('morgan')
 const PORT = 3000;
 
-const argv = yargs
-    .number("id")
-    .string("name")
-    .string("email")
-    .alias("name","n")
-    .string("phone")
-    .argv;
-
-
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-        data.listContacts();    
-      break;
-    case 'get':
-        data.getContactById(id);
-      break;
-
-    case 'add':
-        data.addContact(name, email, phone);
-      break;
-
-    case 'remove':
-        data.removeContact(id);
-      break;
-
-    default:
-      console.warn('\x1B[31m Unknown action type!');
-  }
-}
-
-invokeAction(argv);
-
 const app = express();
-
 app.use(express.json());
 app.use(morgan('tiny'))
 
