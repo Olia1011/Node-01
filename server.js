21e5f368e524f7f2350b0717015be0d36290c4af
Olia1011 <olgamelnyk10111989@gmail.com>
2020-06-30 05:04:25 +0300

    last

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const contactRouter = require("./contacts/contacts.router");
+const contactRouter = require("./api/contacts/contacts.router");
 const morgan = require("morgan");
 
 module.exports = class ContactsServer {
